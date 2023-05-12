6f185fc4915f5425efb85a2ca585136851ff5bed
ObRosen <2542386219@qq.com>
2023-05-13 00:53:11 +0800

    final ver

--- linear_probe_gpu.py
+++ linear_probe_gpu.py
@@ -18,7 +18,8 @@ import warnings
 warnings.filterwarnings("ignore")  # 忽略UserWarning兼容性警告
 
 
-batchSize=128
+batchSize = 128
+
 
 class LinearModel(nn.Module):  # 定义线性模型g,用于c-con
     def __init__(self, input_size):
@@ -42,8 +43,9 @@ def batch_normalize(x, gamma=0.5, beta=0.5, eps=1e-5):
 
 def loss_f1(pred: torch.Tensor, target: torch.Tensor, w, lamb, device: torch.device) -> torch.Tensor:
     mse_loss = torch.mean((pred - target)**2)
-    l1_loss = torch.sum(torch.abs(w))/pred.size(0) # l2_loss = torch.norm(w)/pred.size(0)
-    return mse_loss + lamb * l1_loss # 返回的是平均loss
+    # l2_loss = torch.norm(w)/pred.size(0)
+    l1_loss = torch.sum(torch.abs(w))/pred.size(0)
+    return mse_loss + lamb * l1_loss  # 返回的是平均loss
 
 
 # 定义训练函数
@@ -57,40 +59,43 @@ def train(model: LinearModel, X_train: torch.Tensor, y_train: torch.Tensor, lamb
     y_train = load_tensor(y_train.reshape(-1), batchSize)
 
     for epoch in range(num_epochs):
-        total_loss=0
-        datasize=0
+        total_loss = 0
+        datasize = 0
         for i in range(len(X_train)):
 
             inputs = X_train[i]
             targets = y_train[i]
-            datasize+=targets.size(0)
+            datasize += targets.size(0)
             optimizer.zero_grad()
             outputs = model(inputs)
 
             loss = loss_f1(outputs, targets, model.linear.weight, lamb, device)
-            total_loss+=loss.item()*targets.size(0)
+            total_loss += loss.item()*targets.size(0)
             loss.backward()
             optimizer.step()
             schduler.step(loss)
 
-        if num_epochs>500 and (epoch+1) % 500 == 0:
+        if num_epochs > 500 and (epoch+1) % 500 == 0:
             print('LR: {}, Epoch [{}/{}], Loss: {:.4f}'.format(
                 learning_rate, epoch + 1, num_epochs, total_loss/datasize))
 
 # 正则化参数通过交叉验证确定
+
+
 def decide_lambda_lr(z_d: torch.Tensor, c_z: torch.Tensor, n_splits: int, device: torch.device, random_seed: int = 42):  # z_d为probe的输入，c_z为标签数据
     kf = KFold(n_splits, shuffle=True, random_state=random_seed)
-    lamb_list = [10] # 网格化搜索之后发现lamb的影响很小，为节省时间干脆都定为10
-    lr_list = [1e-06, 1e-07, 1e-08]
-    best_loss_lamb=float('inf')
+    lamb_list = [10]  # 网格化搜索之后发现lamb的影响很小，为节省时间干脆都定为10
+    lr_list = [1e-06, 1e-07, 1e-08, 1e-09]
+    best_loss_lamb = float('inf')
     best_lamb = None
-    best_lamb_lr= None
+    best_lamb_lr = None
 
-    for lamb in lamb_list: #决定了best_lamb及其下的best_loss和best_lr
+    for lamb in lamb_list:  # 决定了best_lamb及其下的best_loss和best_lr
         best_lr = None
         best_loss = float('inf')
-        for lr in lr_list: #决定了当前lamb下的best_loss和best_lr
+        for lr in lr_list:  # 决定了当前lamb下的best_loss和best_lr
             total_loss = 0
+            datasize = 0
             for k, index in enumerate(kf.split(z_d)):
                 train_index, val_index = index
                 X_train, X_val = z_d[train_index], z_d[val_index]
@@ -101,20 +106,22 @@ def decide_lambda_lr(z_d: torch.Tensor, c_z: torch.Tensor, n_splits: int, device
                 linear_model = linear_model.to(device)
                 # print(f'-------------The {k+1}/{n_splits} th training------------')
                 train(linear_model, X_train.reshape(-1, input_size),
-                    y_train.reshape(-1, 1), lamb, device, learning_rate=lr, num_epochs=500)
+                      y_train.reshape(-1, 1), lamb, device, learning_rate=lr, num_epochs=500)
 
                 # 验证
                 inputs = load_tensor(X_val)
                 targets = torch.tensor(y_val, device=device)
                 batch_output = linear_model(inputs[0])
-                for i in range(1,len(inputs)):
-                    batch_output = torch.cat([batch_output,linear_model(inputs[i])],dim=0)
+                for i in range(1, len(inputs)):
+                    batch_output = torch.cat(
+                        [batch_output, linear_model(inputs[i])], dim=0)
 
                 loss = loss_f1(batch_output, targets,
-                            linear_model.linear.weight, lamb, device)
-                total_loss += loss.item()
+                               linear_model.linear.weight, lamb, device)
+                total_loss += loss.item()*targets.size(0)
+                datasize += targets.size(0)
 
-            avg_loss = total_loss / n_splits
+            avg_loss = total_loss / datasize
 
             if avg_loss < best_loss:
                 best_loss = avg_loss
@@ -133,7 +140,7 @@ def decide_lambda_lr(z_d: torch.Tensor, c_z: torch.Tensor, n_splits: int, device
     return lamb, best_lr
 
 
-def concept_probe(concept_name: str, train_val_data: pd.DataFrame, train_zd: torch.Tensor, device: torch.device):
+def concept_probe(concept_name: str, train_val_data: pd.DataFrame, train_zd: torch.Tensor, device: torch.device, num_epochs: int = 1000):
     y_train = []
     for i in range(len(train_val_data)):
         board = train_val_data.iloc[i, :]
@@ -151,7 +158,7 @@ def concept_probe(concept_name: str, train_val_data: pd.DataFrame, train_zd: tor
     linear_model = LinearModel(input_size)
     linear_model = linear_model.to(device)
     train(linear_model, train_zd.reshape(-1, input_size),
-          y_train.reshape(-1, 1), best_lamb, device, best_lr, num_epochs=2000)
+          y_train.reshape(-1, 1), best_lamb, device, best_lr, num_epochs)
     return linear_model
 
 
@@ -176,13 +183,13 @@ def test_accuracy(concept_name: str, model: LinearModel, test_data: pd.DataFrame
 
     # y_test = batch_normalize(y_test) # 该不该正则化？
 
-    test_zd=load_tensor(test_zd)
+    test_zd = load_tensor(test_zd)
     X_test = model(test_zd[0])
-    for i in range(1,len(test_zd)):
-        X_test=torch.cat([X_test,model(test_zd[i])],dim=0)
+    for i in range(1, len(test_zd)):
+        X_test = torch.cat([X_test, model(test_zd[i])], dim=0)
 
     r_squared = r2_score(np.array(y_test), np.array(X_test.tolist()))
-    r_squared_0=r2_score_0(np.array(y_test), np.array(X_test.tolist()))
+    r_squared_0 = r2_score_0(np.array(y_test), np.array(X_test.tolist()))
     print(f"r^2: {r_squared}, r^2_0: {r_squared_0}")
     print("---------------------------------------------------------------\n")
     return r_squared, r_squared_0
@@ -191,7 +198,9 @@ def test_accuracy(concept_name: str, model: LinearModel, test_data: pd.DataFrame
 def compute_results(device: torch.device, file_list, epoch_list, concept, layer_num=15):
     paths = ['./gamedata/WTH_' + str(i)+'.wtb' for i in file_list]
     trainset, testset = read_wthor_files(paths)
-    print(f'使用数据集: {file_list[0]}~{file_list[-1]}, 训练集大小为: {len(trainset)}\n')
+    print(
+        f'Use datasets in years: {file_list[0]}~{file_list[-1]}. Size of trainset: {len(trainset)}')
+    print(f'Concept: {str(concept.__name__)}\n')
     accuracy_step = []
     accuracy_step_0 = []
     for training_step in epoch_list:
@@ -203,7 +212,8 @@ def compute_results(device: torch.device, file_list, epoch_list, concept, layer_
             training_step, into_input_format_2(testset), device)
 
         for layer in range(layer_num):
-            print(f"---------NN training epoch: {training_step}, Layer num: {layer+1}/15---------")
+            print(
+                f"---------NN training epoch: {training_step}, Layer num: {layer+1}/15---------")
             linear_model = concept_probe(
                 concept, trainset, train_z_d_list[layer], device)
             accuracy, accuracy_0 = test_accuracy(
@@ -214,6 +224,7 @@ def compute_results(device: torch.device, file_list, epoch_list, concept, layer_
 
         accuracy_step.append(accuracy_layer)
         accuracy_step_0.append(accuracy_layer_0)
+
     return accuracy_step, accuracy_step_0
 
 
@@ -221,17 +232,23 @@ if __name__ == '__main__':
     globalDevice = detectDevice()
     BLACK = -1
 
-    file_list = range(2021,2022)  # range(1977,2024)
+    file_list = range(2021, 2022)  # range(1977,2024)
     epoch_list = range(1, 58)  # range(1,58)
-    concept = score_f1
-    # , mobility_diff , frontier, frontier_diff, steady, corner, corner_steady, steady_diff, corner_diff, score_f1, score]
+    concept_list = [mobility, mobility_diff, frontier, frontier_diff,
+                    steady, steady_diff, corner, corner_diff, score_f1, score]
 
-    print(f'当前时间: {time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())}')
-    
-    accuracy_step, accuracy_step_0 = compute_results(globalDevice, file_list, epoch_list, concept, 9)
+    for concept in concept_list:
 
-    print_acc(f"concept: {str(concept.__name__)}, accuracy_step: {accuracy_step}")
-    print_acc(f"concept: {str(concept.__name__)}, accuracy_step_0: {accuracy_step_0}")
-    print_acc('---------------------------------------------------------------\n')
+        print(
+            f'Current time: {time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())}')
 
-    # make_plots(concept_list, accuracy_step)
+        accuracy_step, accuracy_step_0 = compute_results(
+            globalDevice, file_list, epoch_list, concept)
+
+        print_acc(f"concept: {str(concept.__name__)}")
+        print_acc(f"accuracy_step: {accuracy_step}")
+        print_acc(f"accuracy_step_0: {accuracy_step_0}")
+        print_acc(
+            '---------------------------------------------------------------\n')
+
+        # make_plots(concept_list, accuracy_step)
--- rewrite_print.py
+++ rewrite_print.py
@@ -4,30 +4,33 @@ import os
 rewrite_print = print
 
 # 定义新的print函数。
+
+
 def print(*arg):
-   # 首先，调用原始的print函数将内容打印到控制台。
-   rewrite_print(*arg)
-   
-   # 如果日志文件所在的目录不存在，则创建一个目录。
-   output_dir = "log_file"
-   if not os.path.exists(output_dir):
-         os.makedirs(output_dir)
-   
-   # 打开（或创建）日志文件并将内容写入其中。
-   log_name = 'log.txt'
-   filename = os.path.join(output_dir, log_name)
-   rewrite_print(*arg,file=open(filename,"a"))
+    # 首先，调用原始的print函数将内容打印到控制台。
+    rewrite_print(*arg)
+
+    # 如果日志文件所在的目录不存在，则创建一个目录。
+    output_dir = "log_file"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # 打开（或创建）日志文件并将内容写入其中。
+    log_name = 'log_detail.txt'
+    filename = os.path.join(output_dir, log_name)
+    rewrite_print(*arg, file=open(filename, "a"))
+
 
 def print_acc(*arg):
-   # 首先，调用原始的print函数将内容打印到控制台。
-   rewrite_print(*arg)
-   
-   # 如果日志文件所在的目录不存在，则创建一个目录。
-   output_dir = "log_file"
-   if not os.path.exists(output_dir):
-         os.makedirs(output_dir)
-   
-   # 打开（或创建）日志文件并将内容写入其中。
-   log_name = 'log_acc.txt'
-   filename = os.path.join(output_dir, log_name)
-   rewrite_print(*arg,file=open(filename,"a"))
+    # 首先，调用原始的print函数将内容打印到控制台。
+    rewrite_print(*arg)
+
+    # 如果日志文件所在的目录不存在，则创建一个目录。
+    output_dir = "log_file"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # 打开（或创建）日志文件并将内容写入其中。
+    log_name = 'log_acc.txt'
+    filename = os.path.join(output_dir, log_name)
+    rewrite_print(*arg, file=open(filename, "a"))
